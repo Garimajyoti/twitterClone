4dac983d933247d467ae5db7928a1a9cc23c33fe
Garima Jyoti <jyotigarima2000@gmail.com>
2023-09-06 19:27:29 -0400

    Components and routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Homepage from './Components/HomePage/Homepage';
+import Authentication from './Components/Authentication/Authentication';
 
 function App() {
   return (
     <div className="">
       
+      <Routes>
+          <Route path="/" element={true? <Homepage/>:<Authentication/>}>
+
+          </Route>
+      </Routes>
     </div>
   );
 }
